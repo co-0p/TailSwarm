a6e8098055dee18e263bbef82b1b0bd53915d1ee
c0-0p <me@c0-0p.io>
2026-01-29 15:29:39 -0800

    cleaned up the deployment step

--- commands/deploy.py
+++ commands/deploy.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 import sys
-from zoneinfo import available_timezones
 
 import click
 import yaml
@@ -9,15 +8,14 @@ import yaml
 from utils.checks import assert_on_correct_tailnet
 from utils.helpers import get_manager_nodes_in_environment, run_remote, simple_run_remote, docker_remote, get_all_node_labels
 
-
-
 @click.command()
+@click.argument('stacks_to_deploy', nargs=-1, required=True)
 @click.option(
     "--environment",
     required=True,
     help="Name of the environment"
 )
-def deploy(environment):
+def deploy(environment, stacks_to_deploy):
     deploy_successful = True
     assert_on_correct_tailnet()
     with open('tailswarm.yml', 'r') as file:
@@ -27,16 +25,22 @@ def deploy(environment):
     environment_spec = config.get("environments", {}).get(environment, {})
     nodes = environment_spec.get("nodes", [])
     stacks = environment_spec.get("stacks", [])
+    deployment_variables = environment_spec.get("deployment-variables", {})
+
+    if not set(stacks_to_deploy).issubset(set(stacks)):
+        click.echo("Error: Not all the given stacks are listed in config file")
+        exit(1)
 
     managers = get_manager_nodes_in_environment(environment)
     manager = managers.pop()
-    print(manager)
 
-    click.echo(f"Deploying to environment {environment} via manager {manager}")
+    click.echo(f"Deploying {len(stacks_to_deploy)} stack(s) to environment '{environment}' via manager node '{manager}'")
 
     # Labels
     environment_node_labels = get_all_node_labels(environment)
 
+    click.echo("")
+    click.echo("Updating node labels...")
     for node in nodes:
         node_name = node.get("name")
         desired_stack_labels = set(node.get("deploy-labels", []))
@@ -50,27 +54,17 @@ def deploy(environment):
         for add in add_stack_labels:
             docker_remote(["docker", "node", "update", "--label-add", f"{add}=true", node_name], manager)
 
-        click.echo(f"To {node_name} added {len(add_stack_labels)} labels, removed {len(rm_stack_labels)}")
+        click.echo(f"  - To node '{node_name}' added {len(add_stack_labels)} labels, removed {len(rm_stack_labels)}")
+    click.echo("")
 
     # Stack deploys
     for stack in stacks:
-        click.echo(f"Deploying stack {stack}...")
-        # cmd = [
-        #     "docker", "stack", "deploy", "--prune", \
-        #     "--detach=false", "--with-registry-auth", \
-        #     "-c", f"dockerswarm/stacks/{stack}.yml", stack
-        # ]
-        # print(" ".join(cmd))
-        # print(docker_run(" ".join(cmd), manager))
+
+        if stack not in stacks_to_deploy:
+            continue
+
+        click.echo(f"Deploying stack '{stack}'. Injecting {len(deployment_variables)} deployment variables.")
         stack_path = os.path.join(docker_stacks_dir, f"{stack.strip()}.yml")
-        # cmd = " ".join([
-        #     "cat", stack_path, "|", \
-        #     "ssh", f"root@{manager}", "docker", "stack", "deploy", "--prune", \
-        #     "--detach=true", "--with-registry-auth", \
-        #     "-c", "-", stack
-        # ])
-        # print(cmd)
-        # run = subprocess.run(cmd, shell=True)
 
 
         args = [
@@ -78,19 +72,14 @@ def deploy(environment):
             "--detach=false", "--with-registry-auth", \
             "-c", stack_path, stack
         ]
-        # run = subprocess.run(
-        #     args,
-        #     capture_output=True, text=True, check=True,
-        #     env={**os.environ, "DOCKER_HOST": f"ssh://root@{node}" }
-        # )
-        print(" ".join(args))
+        # print(" ".join(args))
         try:
-            run = subprocess.run(
-                args,  # Replace with your command and arguments
-                check=True,                        # Raise CalledProcessError on non-zero exit
-                stdout=sys.stdout, #subprocess.PIPE,            # Capture stdout
-                stderr=subprocess.PIPE,             # Capture stderr
-                env={**os.environ, "DOCKER_HOST": f"ssh://root@{manager}" }
+            subprocess.run(
+                args,
+                check=True,
+                stdout=sys.stdout, #subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env={**os.environ, **deployment_variables, "DOCKER_HOST": f"ssh://root@{manager}" }
             )
         except subprocess.CalledProcessError as e:
             deploy_successful = False
@@ -99,8 +88,8 @@ def deploy(environment):
 
         click.echo("")
 
-    click.echo("Stacks deployed")
+    click.echo("Deploy step DONE")
     if not deploy_successful:
-        click.echo("Not everything went right, check logs.")
-    #
-    # # TODO take down unneeded stacks
+        click.echo("NOTE: Not everything went right, check logs.")
+
+    # TODO take down unneeded stacks
